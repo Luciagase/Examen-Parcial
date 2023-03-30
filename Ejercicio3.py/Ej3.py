2dc85dccb207278dfc93c7b089b425cfb0b3962e
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-03-30 09:05:37 +0200

    calificación

--- Ejercicio3.py/Ej3.py
+++ Ejercicio3.py/Ej3.py
@@ -1,5 +1,16 @@
 class Alumno:
-    def __init__(self, nombre, nota):
+    def __init__(self, nombre, nota):#Constructor
         self.nombre=nombre
         self.nota=nota
         print(f'Se ha creado el alumno {self.nombre} con éxito.')
+
+    def calificacion(self):
+        if self.nota >= 5:
+            return f'El alumno {self.nombre} ha aprobado con un {self.nota}.'
+        else:
+            return f'El alumno {self.nombre} ha suspendido con un {self.nota}.'
+        
+    def __str__(self):
+        return f'Alumno: {self.nombre}, Nota: {self.nota}'
+
+    
