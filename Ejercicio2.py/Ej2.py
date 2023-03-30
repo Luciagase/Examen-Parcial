e4206f2a750d46ea2db2d408c1edf55bc59cedb1
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-03-30 08:52:18 +0200

    merge_sort

--- Ejercicio2.py/Ej2.py
+++ Ejercicio2.py/Ej2.py
@@ -9,3 +9,35 @@ class Lista:
             elif num>300:
                 break
         
+    def merge_sort(self):
+        if len(self.lista) <= 1:
+            return self.lista
+        
+        medio = len(self.lista) // 2
+        lista_izquierda=self.lista[:medio]
+        lista_derecha=self.lista[medio:]
+
+        lista_izq_ordenada=Lista(lista_izquierda).merge_sort()
+        lista_der_ordenada=Lista(lista_derecha).merge_sort()
+
+        return Lista.merge(lista_izq_ordenada, lista_der_ordenada)
+
+    @staticmethod
+    def merge(lista_izquierda, lista_derecha):
+        resultado = []
+        i = 0
+        j = 0
+
+        while i<len(lista_izquierda) and j<len(lista_derecha):
+            if lista_izquierda[i] < lista_derecha[j]:
+                resultado.append(lista_izquierda[i])
+                i+=1
+            else:
+                resultado.append(lista_derecha[j])
+                j+=1
+        
+        resultado += lista_izquierda[i:]
+        resultado += lista_derecha[j:]
+
+        return resultado
+
